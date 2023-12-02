e80c74990ae6e0d46f44f41c7240733a3aeafc25
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-02 12:05:31 -0800

    remove unneeded import

--- day01/day01.py
+++ day01/day01.py
@@ -1,18 +1,27 @@
 import argparse
 import regex as re
-import sys
 
 number_word_map = {
-    "zero": "0", "0": "0",
-    "one": "1", "1": "1",
-    "two": "2", "2": "2",
-    "three": "3", "3": "3",
-    "four": "4", "4": "4",
-    "five": "5", "5": "5",
-    "six": "6", "6": "6",
-    "seven": "7", "7": "7",
-    "eight": "8", "8": "8",
-    "nine": "9", "9": "9",
+    "zero": "0",
+    "0": "0",
+    "one": "1",
+    "1": "1",
+    "two": "2",
+    "2": "2",
+    "three": "3",
+    "3": "3",
+    "four": "4",
+    "4": "4",
+    "five": "5",
+    "5": "5",
+    "six": "6",
+    "6": "6",
+    "seven": "7",
+    "7": "7",
+    "eight": "8",
+    "8": "8",
+    "nine": "9",
+    "9": "9",
 }
 
 number_pattern = "|".join(number_word_map.keys())
