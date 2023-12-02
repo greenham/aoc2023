f4b5e7610e9ac3a1068f4a278e78e77a69ae9909
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-02 15:36:00 -0800

    day 2 done

--- day02/day02.py
+++ day02/day02.py
@@ -1,11 +1,12 @@
 import argparse
+import math
 import regex as re
 
 grab_color_pattern = re.compile("(\d+)(red|green|blue)")
 cube_inventory = {"red": 12, "green": 13, "blue": 14}
 
 
-def parse_line(line):
+def parse_game_line(line):
     # Clean up the line a bit and make it easier to parse
     line = line.strip().replace(" ", "")
 
@@ -14,37 +15,62 @@ def parse_line(line):
     game_details = game_parts[1]
 
     # Extract Game ID
-    game_id = re.search("^Game(\d+)", game_parts[0]).group(1)
+    game_id = int(re.search("^Game(\d+)", game_parts[0]).group(1))
 
     # Split out each "grab" from the game details
     game_grabs = game_details.split(";")
 
+    return game_id, game_grabs
+
+
+def get_game_score(game_id, game_grabs):
     # Parse the grabs, seeing if there are any that violate the established count
     for grab in game_grabs:
         sets = grab.split(",")
         for roll in sets:
             roll_seen = re.search(grab_color_pattern, roll)
-            count = roll_seen.group(1)
+            count = int(roll_seen.group(1))
             color = roll_seen.group(2)
-            if int(count) > cube_inventory[color]:
+            if count > cube_inventory[color]:
                 # BAD ROLL
                 print(
                     f"Found impossible roll in game {game_id}: {count} {color} seen, but only {cube_inventory[color]} in inventory"
                 )
                 return 0
 
-    return int(game_id)
+    return game_id
+
+
+def get_cube_set_power(game_grabs):
+    # Track the highest roll we've seen for each color
+    color_minimums = {"red": 0, "green": 0, "blue": 0}
+
+    # Parse the grabs
+    for grab in game_grabs:
+        sets = grab.split(",")
+        for roll in sets:
+            roll_seen = re.search(grab_color_pattern, roll)
+            count = int(roll_seen.group(1))
+            color = roll_seen.group(2)
+            if color_minimums[color] < count:
+                color_minimums[color] = count
+
+    return math.prod(color_minimums.values())
 
 
 def process_file(file_path):
     id_sum = 0
+    power_sum = 0
 
     with open(file_path, "r") as file:
         lines = file.readlines()
         for line in lines:
-            id_sum += parse_line(line)
+            game_id, game_grabs = parse_game_line(line)
+            id_sum += get_game_score(game_id, game_grabs)
+            power_sum += get_cube_set_power(game_grabs)
 
-    return id_sum
+    print(f"Sum of possible game IDs: {id_sum}")
+    print(f"Sum of cube set powers: {power_sum}")
 
 
 def main():
@@ -52,9 +78,7 @@ def main():
     parser.add_argument("input_file_path", help="Path to input data file")
     args = parser.parse_args()
 
-    results = process_file(args.input_file_path)
-
-    print(f"Sum of possible game IDs: {results}")
+    process_file(args.input_file_path)
 
 
 if __name__ == "__main__":
