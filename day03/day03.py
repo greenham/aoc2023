93e9b23309733e47d877d1e42e2c7ed2080ab095
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-03 13:46:41 -0800

    part 1 done

--- day03/day03.py
+++ day03/day03.py
@@ -1,16 +1,57 @@
 import argparse
+import re
 
-
-def parse_game_line(line):
-    return line
+match_numbers = re.compile("\D*(\d+)\D*")
+match_symbols = re.compile("[^0-9.]")
 
 
 def process_file(file_path):
+    part_numbers = []
+
     with open(file_path, "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            line = parse_game_line(line)
-            print(line)
+        file_contents = file.read()
+
+    # Split into lines
+    lines = file_contents.splitlines()
+
+    # Create a matrix of characters
+    char_matrix = [list(line) for line in lines]
+
+    # Find numbers in each line
+    for line_number, line in enumerate(lines, 0):
+        matches = re.finditer(match_numbers, line)
+        for match in matches:
+            number_found = match.group(1)
+            number_start = match.start(1)
+            number_end = match.end(1) - 1
+
+            # print(
+            #     f"Found number {number_found} on line {line_number}, spans {number_start} -> {number_end}"
+            # )
+
+            # Check the appropriate "coordinates" in the matrix to see if there's an "adjacent" symbol
+            x_coords_range = range(number_start - 1, number_end + 2)
+            coords_to_check = (
+                [
+                    (line_number, number_start - 1),
+                    (line_number, number_end + 1),
+                ]
+                + [(line_number - 1, i) for i in x_coords_range]
+                + [(line_number + 1, i) for i in x_coords_range]
+            )
+
+            for x, y in coords_to_check:
+                if 0 <= x < len(char_matrix) and 0 <= y < len(char_matrix[x]):
+                    # print(f"Checking for symbol @ {x}, {y}")
+                    char_at_coord = char_matrix[x][y]
+                    if re.match(match_symbols, char_at_coord):
+                        # print(f"Symbol found: {char_at_coord}")
+                        part_numbers.append(number_found)
+                        break
+
+    part_numbers_int = [int(value) for value in part_numbers]
+    print(f"{len(part_numbers)} part numbers found!")
+    print(f"Sum of part numbers: {sum(part_numbers_int)}")
 
 
 def main():
