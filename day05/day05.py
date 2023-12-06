6cc6ebd298f87a4a6e6183fef6ee4f8402ead5d0
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-05 18:52:35 -0800

    day 5 done

--- day05/day05.py
+++ day05/day05.py
@@ -27,28 +27,54 @@ def process_file(file_path):
             else:
                 number_maps[current_map].append(tuple(map(int, line.split())))
 
-    lowest_location = None
-    current_target = None
+    # Pair the numbers into tuples representing the ranges of seeds (start, length)
+    seed_number_ranges_part1 = [(i, 1) for i in seed_numbers]
+    seed_number_ranges_part2 = list(zip(seed_numbers[::2], seed_numbers[1::2]))
+    print(
+        f"Lowest location (Part 1): {get_lowest_location_for_seeds(seed_number_ranges_part1, number_maps)}"
+    )
+    print(
+        f"Lowest location (Part 2): {get_lowest_location_for_seeds(seed_number_ranges_part2, number_maps)}"
+    )
 
-    for seed_number in seed_numbers:
-        print(f"Searching for location for seed {seed_number}...")
-        current_target = seed_number
-        for key, maps in number_maps.items():
-            print(f"CHECKING MAP: {key} | TARGET: {current_target}")
-            for dest_start, source_start, length in maps:
-                if current_target in range(source_start, source_start + length + 1):
-                    print(f"Found target {current_target} in map {key}!")
 
-                    # This corresponds to the matching entry in the destination range
-                    new_target = dest_start + current_target - source_start
-                    print(f"Setting new target: {new_target}")
-                    current_target = new_target
+def get_lowest_location_for_seeds(seeds, maps):
+    locations = []
+
+    for seed_range in seeds:
+        ranges_to_check = [[seed_range[0], seed_range[1] + seed_range[0]]]
+        results = []
+        for m in maps.values():
+            while ranges_to_check:
+                range_start, range_end = ranges_to_check.pop()
+                for dest_start, source_start, map_length in m:
+                    source_end = source_start + map_length
+                    dest_offset = dest_start - source_start
+
+                    if source_end <= range_start or range_end <= source_start:
+                        # Falls outside of range
+                        continue
+
+                    if range_start < source_start:
+                        # Beginning of range comes before beginning of source
+                        ranges_to_check.append([range_start, source_start])
+                        range_start = source_start
+
+                    if source_end < range_end:
+                        # End of range comes after end of source
+                        ranges_to_check.append([source_end, range_end])
+                        range_end = source_end
+
+                    results.append([range_start + dest_offset, range_end + dest_offset])
                     break
+                else:
+                    results.append([range_start, range_end])
 
-        if lowest_location is None or current_target < lowest_location:
-            lowest_location = current_target
+            ranges_to_check = results
+            results = []
+        locations += ranges_to_check
 
-    print(f"Lowest location found: {lowest_location}")
+    return min(location[0] for location in locations)
 
 
 def main():
