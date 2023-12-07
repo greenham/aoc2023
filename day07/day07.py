e40230ef866f6f143d9a447ee67f04051a6cc533
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-07 00:02:36 -0800

    feel close on p2, maybe just a few more edge cases to handle?

--- day07/day07.py
+++ day07/day07.py
@@ -43,7 +43,7 @@ class Card:
 
 
 class Hand:
-    def __init__(self, value, ref):
+    def __init__(self, value, ref, jokers_wild=False):
         self.value = value
         self.ref = ref
         self.chars = list(value)
@@ -55,30 +55,55 @@ class Hand:
                 char_count[char] += 1
             else:
                 char_count[char] = 1
+
+        # item[0] needs to be a card so the comparison works
         sorted_counts = sorted(
-            char_count.items(), key=lambda item: item[1], reverse=True
+            char_count.items(), key=lambda item: (item[1], Card(item[0])), reverse=True
         )
+
+        print(f"Ranking hand: {self.value}")
+        print(f"Sorted char counts: {sorted_counts}")
+
+        joker_count = 0
+        if jokers_wild and "J" in char_count:
+            joker_count = char_count["J"]
+            print(
+                f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!JOKERS WILD!! (found {joker_count})"
+            )
+
         best_count = sorted_counts[0][1]
+        print(f"Best count: {best_count}")
+
+        # If best_count is 1 or 2 we want to pair our jokers with the highest card?
+        # We're okay with this being == "J" iif best_count is 1
+        if jokers_wild and joker_count > 0:
+            best_card_is_joker = sorted_counts[0][0] == "J"
+            if not best_card_is_joker or best_count == 1:
+                print(
+                    "Best card is NOT a Joker [OR] it IS, and the best_count is 1, so we want to treat this as a pair"
+                )
+                best_count += joker_count
+                print(f"New best count: {best_count}")
+
         if best_count == 5:
             self.hand_type = "five-of-a-kind"
         elif best_count == 4:
             self.hand_type = "four-of-a-kind"
         elif best_count == 3:
-            # max = 3, we have _at least_ 3 of a kind, maybe a full house
             # check for full house
             if (sorted_counts[1][1]) == 2:
                 self.hand_type = "full-house"
             else:
                 self.hand_type = "three-of-a-kind"
         elif best_count == 2:
-            # max = 2, we have _at least_ a pair, maybe 2 pair
-            # check for 2 pair
             if (sorted_counts[1][1]) == 2:
                 self.hand_type = "two-pair"
             else:
                 self.hand_type = "one-pair"
         elif best_count == 1:
             self.hand_type = "high-card"
+
+        print(f"Setting hand type to: {self.hand_type}")
 
         self.rank = hand_rankings[self.hand_type]
 
@@ -128,6 +153,9 @@ class Hand:
 
 
 def process_file(file_path, jokers_wild=False):
+    if jokers_wild:
+        card_value_map["J"] = -1
+
     with open(file_path, "r") as file:
         file_contents = file.read()
         file.close()
@@ -137,7 +165,8 @@ def process_file(file_path, jokers_wild=False):
     data_by_columns = list(zip(*data))
 
     hands = [
-        Hand(hand_string, ref) for ref, hand_string in enumerate(data_by_columns[0])
+        Hand(hand_string, ref, jokers_wild)
+        for ref, hand_string in enumerate(data_by_columns[0])
     ]
     bids = list(map(int, data_by_columns[1]))
 
@@ -149,9 +178,10 @@ def process_file(file_path, jokers_wild=False):
         hand.final_rank = hand_final_rank
         hand.bid = bids[hand.ref]
         hand.winnings = hand.final_rank * hand.bid
-        print(
-            f"winnings for <Hand:{hand.ref}> [{hand.value}]: {hand.winnings} (final rank: {hand.final_rank}, bid: {hand.bid})"
-        )
+        # if "J" in hand.value:
+        #     print(
+        #         f"winnings for <Hand:{hand.ref}> [{hand.value}] ({hand.hand_type}): {hand.winnings} (final rank: {hand.final_rank}, bid: {hand.bid})"
+        #     )
         ranked_hands.append(hand)
         hand_final_rank -= 1
 
@@ -160,7 +190,9 @@ def process_file(file_path, jokers_wild=False):
 
 def main():
     parser = argparse.ArgumentParser(description=f"Advent of Code 2023: Day {DAY}")
-    parser.add_argument("input_file_path", help="Path to input data file")
+    parser.add_argument(
+        "input_file_path", help="Path to input data file", default="example-input"
+    )
     parser.add_argument(
         "-j",
         "--jokers",
