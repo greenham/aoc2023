5333487d9f335bbf75d0d9812b2ad67aa9e94725
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-08 00:14:00 -0800

    closer

--- day08/day08.py
+++ day08/day08.py
@@ -1,8 +1,9 @@
 DAY = 8
 import argparse
+import re
 
 
-def process_file(file_path):
+def process_file(file_path, ghost=False):
     with open(file_path, "r") as file:
         file_contents = file.read()
         file.close()
@@ -17,31 +18,63 @@ def process_file(file_path):
         value_tuple = tuple(map(str.strip, value.strip("()").split(",")))
         node_map[key] = value_tuple
 
-    steps_to_take = directions[:]
-    current_node_id = "AAA"
+    current_step = 0
     steps_taken = 0
-    loops_made = 1
-    for step in steps_to_take:
-        next_element_map = node_map[current_node_id]
-        print(f"Current node: {current_node_id}, going {step}, map: {next_element_map}")
 
-        current_node_id = next_element_map[0] if step == "L" else next_element_map[1]
-        steps_taken += 1
-        print(f"Next node is {current_node_id}! Steps taken: {steps_taken}")
-        if current_node_id == "ZZZ":
-            print("We made it!")
-            break
+    # Part 1
+    if not ghost:
+        current_node_id = "AAA"
+        while True:
+            next_element_map = node_map[current_node_id]
+            print(
+                f"Current node: {current_node_id}, map: {next_element_map}, going {directions[current_step]}"
+            )
+            current_node_id = (
+                next_element_map[0]
+                if directions[current_step] == "L"
+                else next_element_map[1]
+            )
 
-        # handle reaching the end
-        # fix this
-        if steps_taken == len(directions) * (loops_made):
-            print(f"Reached the end, repeating {len(directions)} directions")
-            loops_made += 1
-            steps_to_take.extend(directions)
+            steps_taken += 1
 
-        if loops_made == 500:
-            print("safety break")
-            break
+            if current_node_id == "ZZZ":
+                print("We made it!")
+                break
+
+            # handle incrementing + reaching the end of the directions without having found the end
+            current_step = (current_step + 1) % len(directions)
+
+            print(
+                f"Next node is {current_node_id}, total steps taken so far: {steps_taken}, current step: {current_step}"
+            )
+    # Part 2
+    else:
+        current_nodes = {
+            key: value for key, value in node_map.items() if re.match("^.*A$", key)
+        }
+        print(f"Starting nodes: {current_nodes}")
+        while True:
+            # get the next set of nodes to follow
+            next_maps = current_nodes.values()
+            follow_index = 0 if directions[current_step] == "L" else 1
+            next_node_keys = [item[follow_index] for item in next_maps]
+            steps_taken += 1
+
+            print(f"taking {directions[current_step]} path")
+            print(f"next_node_keys: {next_node_keys}")
+            print(f"steps taken so far: {steps_taken}")
+
+            # see if they all end in Z
+            if all(re.match("^.*Z$", key) for key in next_node_keys):
+                print("We made it!")
+                break
+
+            current_nodes = {
+                key: value for key, value in node_map.items() if key in next_node_keys
+            }
+
+            # handle incrementing + reaching the end of the directions without having found the end
+            current_step = (current_step + 1) % len(directions)
 
     print(f"Total steps taken: {steps_taken}")
 
@@ -51,8 +84,15 @@ def main():
     parser.add_argument(
         "input_file_path", help="Path to input data file", default="example-input"
     )
+    parser.add_argument(
+        "-g",
+        "--ghost",
+        help="Move like a ghost!",
+        action="store_true",
+        default=False,
+    )
     args = parser.parse_args()
-    process_file(args.input_file_path)
+    process_file(args.input_file_path, args.ghost)
 
 
 if __name__ == "__main__":
