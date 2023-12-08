ae2ebf46e021b5f3ba88619bb444780c801304a8
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-07 21:37:12 -0800

    day 8 init

---
+++ day08/day08.py
@@ -0,0 +1,28 @@
+DAY = 8
+import argparse
+
+
+def process_file(file_path, jokers_wild=False):
+    with open(file_path, "r") as file:
+        file_contents = file.read()
+        file.close()
+
+    lines = file_contents.split("\n")
+    directions = lines[0]
+    nodes = lines[2::]
+
+    print(directions)
+    print(nodes)
+
+
+def main():
+    parser = argparse.ArgumentParser(description=f"Advent of Code 2023: Day {DAY}")
+    parser.add_argument(
+        "input_file_path", help="Path to input data file", default="example-input"
+    )
+    args = parser.parse_args()
+    process_file(args.input_file_path)
+
+
+if __name__ == "__main__":
+    main()
