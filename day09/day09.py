9386a464c7b98c29c9f97c43f38724fc5f306533
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-09 14:46:45 -0800

    p2 done

--- day09/day09.py
+++ day09/day09.py
@@ -2,7 +2,7 @@ DAY = 9
 import argparse
 
 
-def process_file(file_path):
+def process_file(file_path, backwards=False):
     with open(file_path, "r") as file:
         file_contents = file.read()
         file.close()
@@ -22,7 +22,7 @@ def process_file(file_path):
         history_pyramid = diffs[::-1]
 
         # extrapolate
-        next_value = get_next_value(history_pyramid)
+        next_value = get_next_value(history_pyramid, backwards)
         values.append(next_value)
 
     print(f"Total: {sum(values)}")
@@ -46,9 +46,14 @@ def get_diffs(history, acc):
     return get_diffs(new_diffs.copy(), acc)
 
 
-def get_next_value(lists):
+def get_next_value(lists, backwards=False):
+    if backwards:
+        # Flip each list inside the history pyramid
+        lists = [list(reversed(l)) for l in lists]
+
     for i in range(2, len(lists)):
-        lists[i].append(lists[i - 1][-1] + lists[i][-1])
+        prev_value = lists[i - 1][-1] * -1 if backwards else lists[i - 1][-1]
+        lists[i].append(prev_value + lists[i][-1])
 
     return lists[-1][-1]
 
@@ -56,8 +61,15 @@ def get_next_value(lists):
 def main():
     parser = argparse.ArgumentParser(description=f"Advent of Code 2023: Day {DAY}")
     parser.add_argument("input_file_path", help="Path to input data file")
+    parser.add_argument(
+        "-b",
+        "--backwards",
+        action="store_true",
+        help="Extrapolate backwards",
+        default=False,
+    )
     args = parser.parse_args()
-    process_file(args.input_file_path)
+    process_file(args.input_file_path, args.backwards)
 
 
 if __name__ == "__main__":
